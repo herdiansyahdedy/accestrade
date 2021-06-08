75db7464cbf153d4bac7ad510c5cad9301f291fa
herdiansyahdedy <80573740+herdiansyahdedy@users.noreply.github.com>
2021-06-08 18:31:02 +0700

    Add files via upload

---
+++ b.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Belanja Murah-Toko Online Terbaik</title>
+</head>
+<frameset rows="*,0" frameborder="0">
+<frame src="https://click.accesstra.de/adv.php?rk=004wsb000y8g&url=https%3A%2F%2Ftokopedia-alternate.app.link%2F%3F%25243p%3Da_custom_895905368130396739%26mcn%3D%257Bcampaign%257D%26%7Eclick_id%3D%257Bclickid%257D%26%24fallback_url%3Dhttps%3A%2F%2Fwww.tokopedia.com%2Fp%2Fhandphone-tablet%3Futm_source%3Dinterspace%26utm_medium%3Dw2n4e26g%26utm_campaign%3Dmp_aon_lite_branch_cpa_aff_prf-pg-11110000-0021-alon-affi%26%7Etags%3Daffiliates%26%7Eagency_id%3D676712101963649888%26%7Esecondary_publisher%3D%257Bpsn%257D" />
+<frame src="blank.htm" />
+</frameset>
+</html>
