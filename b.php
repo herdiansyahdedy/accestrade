05b156f30606de43348aa10016b5f44056587230
herdiansyahdedy <80573740+herdiansyahdedy@users.noreply.github.com>
2021-06-10 23:31:19 +0700

    Update b.php

--- b.php
+++ b.php
@@ -1,35 +1,9 @@
-
-
-<!DOCTYPE html><html lang="en-US"><head><meta charset="UTF-8"><link media="all" href="https://click.accesstra.de/adv.php?rk=004wsb000y8g&url=https%3A%2F%2Ftokopedia-alternate.app.link%2F%3F%25243p%3Da_custom_895905368130396739%26mcn%3D%257Bcampaign%257D%26%7Eclick_id%3D%257Bclickid%257D%26%24fallback_url%3Dhttps%3A%2F%2Fwww.tokopedia.com%2Fp%2Fhandphone-tablet%3Futm_source%3Dinterspace%26utm_medium%3Dw2n4e26g%26utm_campaign%3Dmp_aon_lite_branch_cpa_aff_prf-pg-11110000-0021-alon-affi%26%7Etags%3Daffiliates%26%7Eagency_id%3D676712101963649888%26%7Esecondary_publisher%3D%257Bpsn%257D" rel="stylesheet" /><link media="print" href="https://www.shopinthe.site/wp-content/c/css/hantamo_f637c8737ec7525695a5301f7a69ccd9.css" rel="stylesheet" /><title>Belanja Online &#8211; Toko Online Terpercaya</title><link href='https://fonts.gstatic.com' crossorigin='anonymous' rel='preconnect' /><link rel="alternate" type="application/rss+xml" title="Herbal Alami Terpercaya &raquo; Feed" href="https://www.shopinthe.site/feed/" /><link rel="alternate" type="application/rss+xml" title="Herbal Alami Terpercaya &raquo; Comments Feed" href="https://www.shopinthe.site/comments/feed/" /> <script id='wk-tag-manager-script-js-after'>function shouldTrack(){
-var trackLoggedIn = false;
-var loggedIn = false;
-if(!loggedIn){
-return true;
-} else if( trackLoggedIn ) {
-return true;
-}
-return false;
-}
-function hasWKGoogleAnalyticsCookie() {
-return (new RegExp('wp_wk_ga_untrack_' + document.location.hostname)).test(document.cookie);
-}
-if (!hasWKGoogleAnalyticsCookie() && shouldTrack()) {
-//Google Tag Manager
-(function (w, d, s, l, i) {
-w[l] = w[l] || [];
-w[l].push({
-'gtm.start':
-new Date().getTime(), event: 'gtm.js'
-});
-var f = d.getElementsByTagName(s)[0],
-j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-j.async = true;
-j.src =
-'//www.googletagmanager.com/gtm.js?id=' + i + dl;
-f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-PPQCQB7');
-}</script> <link rel="https://api.w.org/" href="https://click.accesstra.de/adv.php?rk=004wsb000y8g&url=https%3A%2F%2Ftokopedia-alternate.app.link%2F%3F%25243p%3Da_custom_895905368130396739%26mcn%3D%257Bcampaign%257D%26%7Eclick_id%3D%257Bclickid%257D%26%24fallback_url%3Dhttps%3A%2F%2Fwww.tokopedia.com%2Fp%2Fhandphone-tablet%3Futm_source%3Dinterspace%26utm_medium%3Dw2n4e26g%26utm_campaign%3Dmp_aon_lite_branch_cpa_aff_prf-pg-11110000-0021-alon-affi%26%7Etags%3Daffiliates%26%7Eagency_id%3D676712101963649888%26%7Esecondary_publisher%3D%257Bpsn%257D {
-		document.body.classList.add( 'is-IE' );
-	}</script> <noscript><style>.lazyload{display:none;}</style></noscript><script data-noptimize="1">window.lazySizesConfig=window.lazySizesConfig||{};window.lazySizesConfig.loadMode=1;</script><script async data-noptimize="1" src='https://click.accesstra.de/adv.php?rk=004wsb000y8g&url=https%3A%2F%2Ftokopedia-alternate.app.link%2F%3F%25243p%3Da_custom_895905368130396739%26mcn%3D%257Bcampaign%257D%26%7Eclick_id%3D%257Bclickid%257D%26%24fallback_url%3Dhttps%3A%2F%2Fwww.tokopedia.com%2Fp%2Fhandphone-tablet%3Futm_source%3Dinterspace%26utm_medium%3Dw2n4e26g%26utm_campaign%3Dmp_aon_lite_branch_cpa_aff_prf-pg-11110000-0021-alon-affi%26%7Etags%3Daffiliates%26%7Eagency_id%3D676712101963649888%26%7Esecondary_publisher%3D%257Bpsn%257D'></script><script id='twenty-twenty-one-ie11-polyfills-js-after' data-type="lazy" data-src="data:text/javascript;base64,KCBFbGVtZW50LnByb3RvdHlwZS5tYXRjaGVzICYmIEVsZW1lbnQucHJvdG90eXBlLmNsb3Nlc3QgJiYgd2luZG93Lk5vZGVMaXN0ICYmIE5vZGVMaXN0LnByb3RvdHlwZS5mb3JFYWNoICkgfHwgZG9jdW1lbnQud3JpdGUoICc8c2NyaXB0IHNyYz0iaHR0cHM6Ly93d3cuc2hvcGludGhlLnNpdGUvd3AtY29udGVudC9jL2pzL2hhbnRhbW9fc2luZ2xlXzNlODVjMGE3NTdmNDhhNWM5MDJlMTk5Yjk1NDEyMDJlLmpzP3Zlcj0xLjEiPjwvc2NyJyArICdpcHQ+JyApOw=="></script> <script   id='elementor-pro-frontend-js-before'>var ElementorProFrontendConfig = {"ajaxurl":"https:\/\/www.shopinthe.site\/wp-admin\/admin-ajax.php","nonce":"ae33d3ee83","i18n":{"toc_no_headings_found":"No headings were found on this page."},"shareButtonsNetworks":{"facebook":{"title":"Facebook","has_counter":true},"twitter":{"title":"Twitter"},"google":{"title":"Google+","has_counter":true},"linkedin":{"title":"LinkedIn","has_counter":true},"pinterest":{"title":"Pinterest","has_counter":true},"reddit":{"title":"Reddit","has_counter":true},"vk":{"title":"VK","has_counter":true},"odnoklassniki":{"title":"OK","has_counter":true},"tumblr":{"title":"Tumblr"},"digg":{"title":"Digg"},"skype":{"title":"Skype"},"stumbleupon":{"title":"StumbleUpon","has_counter":true},"mix":{"title":"Mix"},"telegram":{"title":"Telegram"},"pocket":{"title":"Pocket","has_counter":true},"xing":{"title":"XING","has_counter":true},"whatsapp":{"title":"WhatsApp"},"email":{"title":"Email"},"print":{"title":"Print"}},"facebook_sdk":{"lang":"en_US","app_id":""},"lottie":{"defaultAnimationUrl":"https:\/\/www.shopinthe.site\/wp-content\/plugins\/elementor-pro\/modules\/lottie\/assets\/animations\/default.json"}};</script> <script   id='elementor-frontend-js-before'>var elementorFrontendConfig = {"environmentMode":{"edit":false,"wpPreview":false,"isScriptDebug":false,"isImprovedAssetsLoading":false},"i18n":{"shareOnFacebook":"Share on Facebook","shareOnTwitter":"Share on Twitter","pinIt":"Pin it","download":"Download","downloadImage":"Download image","fullscreen":"Fullscreen","zoom":"Zoom","share":"Share","playVideo":"Play Video","previous":"Previous","next":"Next","close":"Close"},"is_rtl":false,"breakpoints":{"xs":0,"sm":480,"md":768,"lg":1025,"xl":1440,"xxl":1600},"version":"3.1.1","is_static":false,"experimentalFeatures":{"e_dom_optimization":true,"a11y_improvements":true,"landing-pages":true},"urls":{"assets":"https:\/\/www.shopinthe.site\/wp-content\/plugins\/elementor\/assets\/"},"settings":{"page":[],"editorPreferences":[]},"kit":{"global_image_lightbox":"yes","lightbox_enable_counter":"yes","lightbox_enable_fullscreen":"yes","lightbox_enable_zoom":"yes","lightbox_enable_share":"yes","lightbox_title_src":"title","lightbox_description_src":"description"},"post":{"id":154,"title":"PRIMATON%20%E2%80%93%20Herbal%20Alami%20Terpercaya","excerpt":"","featuredImage":false}};</script> <script type="text/javascript" id="flying-scripts">const loadScriptsTimer=setTimeout(loadScripts,5*1000);const userInteractionEvents=["mouseover","keydown","touchmove","touchstart"];userInteractionEvents.forEach(function(event){window.addEventListener(event,triggerScriptLoader,{passive:!0})});function triggerScriptLoader(){loadScripts();clearTimeout(loadScriptsTimer);userInteractionEvents.forEach(function(event){window.removeEventListener(event,triggerScriptLoader,{passive:!0})})}
-function loadScripts(){document.querySelectorAll("script[data-type='lazy']").forEach(function(elem){elem.setAttribute("src",elem.getAttribute("data-src"))})}</script> <script>/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",(function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())}),!1);</script> <script defer data-type="lazy" data-src="https://click.accesstra.de/adv.php?rk=004wsb000y8g&url=https%3A%2F%2Ftokopedia-alternate.app.link%2F%3F%25243p%3Da_custom_895905368130396739%26mcn%3D%257Bcampaign%257D%26%7Eclick_id%3D%257Bclickid%257D%26%24fallback_url%3Dhttps%3A%2F%2Fwww.tokopedia.com%2Fp%2Fhandphone-tablet%3Futm_source%3Dinterspace%26utm_medium%3Dw2n4e26g%26utm_campaign%3Dmp_aon_lite_branch_cpa_aff_prf-pg-11110000-0021-alon-affi%26%7Etags%3Daffiliates%26%7Eagency_id%3D676712101963649888%26%7Esecondary_publisher%3D%257Bpsn%257D"></script></body></html>
-<!-- Page generated by LiteSpeed Cache 3.6.2 on 2021-04-07 19:13:20 -->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Store Clothing Shirts</title>
+<meta http-equiv="refresh" content="0;URL='https://click.accesstra.de/adv.php?rk=004wsb000y8g&url=https%3A%2F%2Ftokopedia-alternate.app.link%2F%3F%25243p%3Da_custom_895905368130396739%26mcn%3D%257Bcampaign%257D%26%7Eclick_id%3D%257Bclickid%257D%26%24fallback_url%3Dhttps%3A%2F%2Fwww.tokopedia.com%2Fp%2Fhandphone-tablet%3Futm_source%3Dinterspace%26utm_medium%3Dw2n4e26g%26utm_campaign%3Dmp_aon_lite_branch_cpa_aff_prf-pg-11110000-0021-alon-affi%26%7Etags%3Daffiliates%26%7Eagency_id%3D676712101963649888%26%7Esecondary_publisher%3D%257Bpsn%257D'" />
+</head>
+<body>
+</body>
+</html>
