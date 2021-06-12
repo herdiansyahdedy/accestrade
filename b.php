7c957fc517f24ccd099f7fab731710309af50fe7
herdiansyahdedy <80573740+herdiansyahdedy@users.noreply.github.com>
2021-06-12 22:56:24 +0700

    Update b.php

--- b.php
+++ b.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <title>Store Smartphone Terpercaya</title>
-<meta http-equiv="refresh" content="0;URL='https://click.accesstra.de/adv.php?rk=004wsb000y8g&url=https%3A%2F%2Ftokopedia-alternate.app.link%2F%3F%25243p%3Da_custom_895905368130396739%26mcn%3D%257Bcampaign%257D%26%7Eclick_id%3D%257Bclickid%257D%26%24fallback_url%3Dhttps%3A%2F%2Fwww.tokopedia.com%2Fp%2Fhandphone-tablet%3Futm_source%3Dinterspace%26utm_medium%3Dw2n4e26g%26utm_campaign%3Dmp_aon_lite_branch_cpa_aff_prf-pg-11110000-0021-alon-affi%26%7Etags%3Daffiliates%26%7Eagency_id%3D676712101963649888%26%7Esecondary_publisher%3D%257Bpsn%257D'" />
+<meta http-equiv="refresh" content="0;URL='https://click.accesstra.de/adv.php?rk=004xyg000y8g&url=https%3A%2F%2Ftokopedia-alternate.app.link%2F%3F%25243p%3Da_custom_895905368130396739%26mcn%3D%257Bcampaign%257D%26%7Eclick_id%3D%257Bclickid%257D%26%24fallback_url%3Dhttps%3A%2F%2Fwww.tokopedia.com%2Fxiaomi%2F%3Futm_source%3Dinterspace%26utm_medium%3Dw2n4e26g%26utm_campaign%3Dmp_aon_lite_branch_cpa_aff_prf-pg-11110000-0021-alon-affi%26%7Etags%3Daffiliates%26%7Eagency_id%3D676712101963649888%26%7Esecondary_publisher%3D%257Bpsn%257D'" />
 </head>
 <body>
 </body>
